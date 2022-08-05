6bb3a95d5212a09c4dfcb4d9f46b15db340449d2
eahb0y <getelementby111@gmail.com>
2022-08-05 22:27:37 +0500

    created and showed all db, API works

---
+++ greet/greeting.go
@@ -0,0 +1,18 @@
+package greet
+
+import (
+	"fmt"
+)
+
+const pi = 3.14
+
+func PrintCircleArea(radius int) {
+	fmt.Printf("Радиус круга %d", radius)
+	fmt.Println("Формула падиуса круга 2`2*pi")
+
+	circleArea := CalculateCricleArea(radius)
+	fmt.Printf("Плошад круга %f32 = ", circleArea)
+}
+func CalculateCricleArea(radius int) float32 {
+	return float32(radius) * float32(radius) * pi
+}
--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"first_rep/greet"
+	"fmt"
+	"log"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gofiber/fiber/v2"
+	"github.com/jmoiron/sqlx"
 )
 
 // var schema = `
@@ -17,32 +22,48 @@ import (
 // 	)
 // `
 
-// type Pet struct {
-// 	ID        int     `db:"id"`
-// 	Name      string  `db:"name"`
-// 	Age       int     `db:"age"`
-// 	Gender    string  `db:"gender"`
-// 	CanFly    bool    `db:"can_fly"`
-// 	CreatedAt *string `db:"created_at"`
-// 	UpdateAt  *string `db:"updated_at"`
-// }
-func main() {
-	app := fiber.New()
+type Pet struct {
+	ID        int     `db:"id" json:"id"`
+	Name      string  `db:"name" json:"name" query:"name"`
+	Age       int     `db:"age" json:"age" query:"age"`
+	Gender    string  `db:"gender" json:"gender" query:"gender"`
+	CanFly    bool    `db:"can_fly" json:"can_fly" query:"can_fly"`
+	CreatedAt *string `db:"created_at" json:"created_at"`
+	UpdateAt  *string `db:"updated_at" json:"updated_at"`
+}
 
+func main() {
+
+	config := fmt.Sprintf(
+		"%s:%s@tcp(%s)/%s?parseTime=True",
+		"root", "root", "127.0.0.2", "animals",
+	)
+	db, err := sqlx.Connect("mysql", config)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	app := fiber.New()
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
-
+	app.Get("animals", func(c *fiber.Ctx) error {
+		animals := []Pet{}
+		err = db.Select(&animals, "SELECT * FROM pets")
+		if err != nil {
+			log.Println(err)
+		}
+		return c.JSON(animals)
+	})
+	app.Get("animals/add", func(ctx *fiber.Ctx) error {
+		pet := Pet{}
+		ctx.QueryParser(&pet)
+		tx := db.MustBegin()
+		tx.MustExec("INSERT INTO pets (name, age, gender, can_fly) VALUES (?, ?, ?, ?)", pet.Name, pet.Age, pet.Gender, pet.CanFly)
+		tx.Commit()
+		return ctx.JSON(pet)
+	})
 	app.Listen(":3000")
-	// config := fmt.Sprintf(
-	// 	"%s:%s@tcp(%s)/%s?parseTime=True",
-	// 	"root", "root", "127.0.0.2", "animals",
-	// )
-	// db, err := sqlx.Connect("mysql", config)
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
-
 	// // exec the schema or fail; multi-statement Exec behavior varies between
 	// // database drivers;  pq will exec them all, sqlite3 won't, ymmv
 	// db.MustExec(schema)
@@ -53,14 +74,12 @@ func main() {
 	// // tx.MustExec("INSERT INTO pets (name, age, gender, can_fly) VALUES (?, ?, ?, ?)", "Dog", 3, "male", false)
 	// // tx.MustExec("INSERT INTO pets (name, age, gender, can_fly) VALUES (?, ?, ?, ?)", "Something", 50, "female", true)
 	// // tx.Commit()
-	// animals := []Pet{}
-	// err = db.Select(&animals, "SELECT * FROM pets")
-	// if err != nil {
-	// 	log.Println(err)
-	// }
+
 	// for _, v := range animals {
 	// 	log.Println(v)
 	// }
 	// fmt.Print("he")
+	greet.PrintCircleArea(6)
+	greet.CalculateCricleArea(5)
 
 }
