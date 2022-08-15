370769cd8e9a81655a6cd639026f4522cf89df6d
eahb0y <getelementby111@gmail.com>
2022-08-16 01:35:54 +0500

    commented unusing code

--- main.go
+++ main.go
@@ -1,40 +1,71 @@
 package main
 
 import (
-	"fmt"
-	"log"
-
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/jmoiron/sqlx"
 )
 
 //database
-var customers = `
-	CREATE TABLE IF NOT EXISTS customers (
-		id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-		name VARCHAR(255),
-		age INT(6) UNSIGNED,
-		gender ENUM('male', 'female'),
-		can_fly BOOLEAN,
-		created_at DATETIME,
-		updated_at TIMESTAMP
-		pet_id INT(2) UNSIGNED,
-	)
-`
-var tables = `
-	CREATE TABLE IF NOT EXISTS tables(
+
+var customer_plan = `
+	CREATE TABLE IF NOT EXISTS customer_plan(
 		id INT(2) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-		type VARCHAR(255),
-		column_id INT(6) UNSIGNED
+		customers_id INT(2),
+		plan_id INT(2)
 	)
 `
 
-const customer_plan = `
-	CREATE TABLE IF NOT EXISTS costumer_plan(
-		id INT(2) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-		working_day VARCHAR(255),
-		resting_day VARCHAR(255)
+var tables = `
+	CREATE TABLE IF NOT EXISTS tables(
+		id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+		number INT(2) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+		table_type_id INT(2),
+		room_id INT(2)
 	)
+`
+var customer_table = `
+	CREATE TABLE IF NOT EXISTS customer_table(
+		customer_id INT(2),
+		table_id INT(2)
+	)
+`
+
+var table_types = `
+	CREATE TABLE IF NOT EXISTS table_types(
+		id INT(2) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+		type VARCHAR(255)
+	)
+`
+
+var customer_room = `
+	CREATE TABLE IF NOT EXISTS customer_room(
+		customer_id INT(2),
+		room_id INT(2)
+	)
+`
+
+var rooms = `
+	CREATE TABLE IF NOT EXISTS rooms(
+		id INT(2),
+		name VARCHAR(255),
+		working_pleace_count INT(2)
+	)
+`
+
+var plans = `
+	CREATE TABLE IF NOT EXISTS plans(
+		id INT(2) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+		name VARCHAR(255),
+		price INT(6)
+	)
+`
+
+var customers = `
+CREATE TABLE IF NOT EXISTS customers(
+	id INT(2) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	first_name VARCHAR(255),
+	last_name VARCHAR(255),
+	passport_id INT(6)
+)
 `
 
 //database struct
@@ -57,15 +88,19 @@ type pets_type struct {
 
 //main function
 func main() {
+	//declare databese
 
-	config := fmt.Sprintf(
-		"%s:%s@tcp(%s)/%s?parseTime=True",
-		"root", "root", "127.0.0.2", "animals",
-	)
-	db, err := sqlx.Connect("mysql", config)
-	if err != nil {
-		log.Fatalln(err)
-	}
+	// config := fmt.Sprintf(
+	// 	"%s:%s@tcp(%s)/%s?parseTime=True",
+	// 	"root", "root", "127.0.0.2", "animals",
+	// )
+	// db, err := sqlx.Connect("mysql", config)
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+
+	//using fiber for API
+
 	// app := fiber.New()
 	// app.Get("/", func(c *fiber.Ctx) error {
 	// 	return c.SendString("Hello, World!")
@@ -88,11 +123,14 @@ func main() {
 	// })
 	// app.Listen(":3000")
 
+	//enter to db values
 	// exec the schema or fail; multi-statement Exec behavior varies between
-	// database drivers;  pq will exec them all, sqlite3 won't, ymmv
-	// db.MustExec(schema)
-	db.MustExec(customer_plan)
-	tx := db.MustBegin()
-	tx.Commit()
+	// // database drivers;  pq will exec them all, sqlite3 won't, ymmv
+	// db.MustExec()
+	// db.MustExec(customers)
+	// tx := db.MustBegin()
+	// db.MustExec("INSERT INTO customers(first_name, last_name, passport_id) VALUES (?, ?, ?)", "Robert", "Downey", 21038723)
+	// db.MustExec("INSERT INTO customers(first_name, last_name, passport_id) VALUES (?, ?, ?)", "Robert", "Downey", 21038723)
+	// tx.Commit()
 
 }
